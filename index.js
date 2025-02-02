a973c0b186d72d2cad9c98288fe28d8fe1f26096
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-02-02 22:58:49 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+// index.js
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ name, email })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Appends the id to the DOM (for testing purposes)
+      document.body.innerHTML = data.id;
+    })
+    .catch(error => {
+      // Appends error message to the DOM (for testing purposes)
+      document.body.innerHTML = error.message;
+    });
+  }
+  
+  // Export the function for use in tests
+  module.exports = submitData;
+  
